307c3589572c3341d57f9354e6c79354153124b6
allon21 <anthon.chub@yandex.ru>
2025-02-12 17:28:12 +0300

    ref

--- server/src/main/java/ru/practicum/shareit/user/service/UserServiceImpl.java
+++ server/src/main/java/ru/practicum/shareit/user/service/UserServiceImpl.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.shareit.exception.EmailExistException;
 import ru.practicum.shareit.exception.NotFoundException;
-import ru.practicum.shareit.exception.ValidationException;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.dto.UserMapper;
 import ru.practicum.shareit.user.model.User;
